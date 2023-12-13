bd9e787ab24a3030b12687e06bb5d5ce26daa429
aianafitlife <145410386+aianafitlife@users.noreply.github.com>
2023-12-13 23:38:53 +0600

    change class name

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { MdOutlineFileDownloadDone} from "react-icons/md";
 import './App.css'
-import {completed} from "./utilsFunctions";
-import { getNewTask } from './getNewTaskApi';
+import {getNewTask} from "./getNewTaskApi";
 
 const Task = ({task, setTask, setIsOpen}) => {
-
+  const completed = JSON.parse(localStorage.getItem('completed'))
   const handleCompleteTask = () => {
     localStorage.setItem('completed', JSON.stringify([...completed, task]))
     getNewTask(setTask)
@@ -16,7 +15,7 @@ const Task = ({task, setTask, setIsOpen}) => {
     <div className='item'>
       {task?.todo}
       {
-        !completed?.some(el => task?.id === el?.id) && <MdOutlineFileDownloadDone onClick={handleCompleteTask}/>
+        !completed?.some(el => task?.id === el?.id) && <MdOutlineFileDownloadDone className='check' onClick={handleCompleteTask}/>
       }
     </div>
   );
