7d949dfe31370e7833bf22786d1cb26c0afeda3f
aianafitlife <145410386+aianafitlife@users.noreply.github.com>
2023-12-13 23:38:40 +0600

    fix request bug

--- src/finishedTasks.jsx
+++ src/finishedTasks.jsx
@@ -3,11 +3,12 @@ import Task from "./Task";
 import {completed} from "./utilsFunctions";
 
 const FinishedTasks = () => {
+  const tasks = JSON.parse(localStorage.getItem('completed'))
   return (
     <div className='finished'>
       <h5 className='title'>Completed tasks</h5>
       {
-        completed?.map((el) => (
+        tasks?.map((el) => (
           <Task task={el}/>
         ))
       }
